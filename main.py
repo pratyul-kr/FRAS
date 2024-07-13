fc74550f4e254d287bd1b031e33dcfd49f6b0e24
Pratyul Kumar Rath <pratyulrath1234@gmail.com>
2024-07-12 20:52:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,73 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+#import libraries
+import cv2
+import numpy as np
+import csv
 from datetime import datetime
-import json
+import face_recognition
+import face_recognition_models
 
-with open('config.json', 'r') as c:
-    params = json.load(c)("params")
+video_capture = cv2.VideoCapture(0)
 
-local_server = True
-app = Flask(__name__)
-if (local_server):
-    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+#Load known faces
+user1_image = face_recognition.load_image_file("faces/user1.jpg")
+user1_encoding = face_recognition.face_encodings(user1_image)[0]
 
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+user2_image = face_recognition.load_image_file("faces/user2.jpg")
+user2_encoding = face_recognition.face_encodings(user2_image)[0]
 
-db = SQLAlchemy(app)
+known_face_encodings = [user1_encoding, user2_encoding]
+known_face_names = ["User1", "User2"]
 
+#list of expected users
+users = known_face_names.copy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    face = db.Column(db.String(100), nullable=False)
-    datetime = db.Column(db.String(20), nullable=True)
+face_locations = []
+face_encodings = []
 
+#get current time and date
 
-@app.route("/")
-def hello():
-    return 'Success!'
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
 
+f = open(f"{current_date}.csv", "w+", newline="")
+lnwriter = csv.written(f)
 
-@app.route("/homepage", method=['GET', 'POST'])
-def homepage():
-    if (request.method == 'POST'):
-        name = request.form.get('name')
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
-        entry = User(name=name, datetime=datetime.now())
-        db.session.add(entry)
-        db.session.commit()
+    #Recognize faces
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    return render_template('index.html')
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distance)
 
+        if(matches[best_match_index]):
+            name = known_face_names[best_match_index]
 
-app.run(debug=True)
+        #Add text if a user is present
+        if name in known_face_names:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            bottomLeftCornerOfText = (10, 100)
+            fontScale = 1.5
+            fontColor = (255, 0 , 0)
+            thickness = 3
+            lineType = 2
+            cv2.putText(frame, name + " Present", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+
+            if name in users:
+                users.remove(name)
+                current_time = now.strftime("%H-%M-%S")
+                lnwriter.writerrow(name, current_time)
+
+    cv2.imshow("Attendance", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
